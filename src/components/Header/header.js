dbfe08f2269733ee30afc877e91a670d48b96117
Alexis Edwards <101596613+ASP22SCM65E@users.noreply.github.com>
2023-06-11 20:34:55 -0500

    Firebase Hosting

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import "../../styles/components/Header.css"
+import "../../styles/pages/Header.css"
 
 import logo from "../../images/logo-combined.png";
 
