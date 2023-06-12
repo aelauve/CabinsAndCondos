2898d5df133c8b52cf8a306daf8f42e3cd7e5f84
Alexis Edwards <101596613+ASP22SCM65E@users.noreply.github.com>
2023-06-11 22:35:34 -0500

    Test

---
+++ src/components/table-date-picker.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function TableDatePicker() {
+ const [date, setDate] = useState(new Date());
+ const [endDate, setEndDate] = useState(new Date());
+
+ return (
+    <div>
+        <DatePicker
+            selected={startDate}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            onChange={date => setStartDate(date)}
+        />
+        <DatePicker
+            selected={endDate}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            onChange={date => setEndDate(date)}
+        />
+  </div>
+ );
+}
