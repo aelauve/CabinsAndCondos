73700c11e262e81597d1875cfe3a4355343e3fed
Alexis Edwards <101596613+ASP22SCM65E@users.noreply.github.com>
2023-06-12 21:12:27 -0500

    Duplicate Header

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -12,39 +12,37 @@ import logCabin from "../images/cabin4.jpeg";
 
 function Properties() {
   return (
-    <>
-        <main>
-            <section className="main-content">
-                <PropertyFilter />
-                <LocationTitle location="Destin, FL" />
-                <div className="property-flexbox">
-                    <PropertyCard 
-                    title="West Pelican" 
-                    imgSource={westPelican}
-                    price="500"
-                    />
-                    <PropertyCard 
-                    title="Sunny Days" 
-                    imgSource={sunnyDays}
-                    price="425"
-                    />
-                </div>
-                <LocationTitle location="Gatlinburg, TN" />
-                <div className="property-flexbox">
-                    <PropertyCard 
-                    title="All Knotty" 
-                    imgSource={allKnotty}
-                    price="375"
-                    />
-                    <PropertyCard 
-                    title="Log Cabin" 
-                    imgSource={logCabin}
-                    price="350"
-                    />
-                </div>
-            </section>
-        </main>
-    </>
+    <main>
+        <section className="main-content">
+            <PropertyFilter />
+            <LocationTitle location="Destin, FL" />
+            <div className="property-flexbox">
+                <PropertyCard 
+                title="West Pelican" 
+                imgSource={westPelican}
+                price="500"
+                />
+                <PropertyCard 
+                title="Sunny Days" 
+                imgSource={sunnyDays}
+                price="425"
+                />
+            </div>
+            <LocationTitle location="Gatlinburg, TN" />
+            <div className="property-flexbox">
+                <PropertyCard 
+                title="All Knotty" 
+                imgSource={allKnotty}
+                price="375"
+                />
+                <PropertyCard 
+                title="Log Cabin" 
+                imgSource={logCabin}
+                price="350"
+                />
+            </div>
+        </section>
+    </main>
   )
 }
 
